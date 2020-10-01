dd7bd375c55a4cad0839d29ba3a630d902894fe6
lulu2002 <lose9749@gmail.com>
2020-10-01 23:26:58 +0800

    重寫 Footer，完成個人連結

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,4 +4,6 @@ import {Injectable} from '@angular/core';
 export class Globals {
   igLink = 'https://www.instagram.com/ypbc_6th/';
   fbLink = 'https://www.facebook.com/691757424602455/';
+  sourceLink = 'https://github.com/lulu2002/YPBC-Website';
+  authorLink = 'https://www.instagram.com/lin_911006/';
 }
